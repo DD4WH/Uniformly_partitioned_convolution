40516e7920808a5b4bdb2fabe0618b5258824417
dd4wh <dd4wh.swl@gmail.com>
2019-12-13 08:35:40 +0100

    correct licenses

--- guitar_Teensy_3_6/guitar_Teensy_3_6.ino
+++ guitar_Teensy_3_6/guitar_Teensy_3_6.ino
@@ -12,7 +12,24 @@
  * inspired by and uses code from wdsp library by Warren Pratt
  * https://github.com/g0orx/wdsp/blob/master/firmin.c
  * 
- * in the public domain GNU GPL v3
+*********************************************************************************
+
+   GNU GPL LICENSE v3
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+ *********************************************************************************
  */
 
 // processor load Teensy 3.6 with 180MHz
